b78106c65e1bd87551e2a64812bc7def628696a8
mintmin <mintmin0320@gmail.com>
2022-12-18 02:53:21 +0900

    Update login page

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import { AppDataSource } from './data-source';
 const express = require('express');
 const morgan = require('morgan');
+import authRoute from './routes/auth'
+const cors = require('cors');
 
 const app = express();
+const origin = "http://localhost:3000";
+app.use(cors({
+  origin
+}));
 
 app.use(express.json())
 app.use(morgan('dev'));
 
 app.get("/", (_, res) => res.send("running"));
+app.use("/api/auth", authRoute);
 
 let port = 4000;
 app.listen(port, async () => {
