ed96343d72468a1929b9b7c41ef17d4bb2f49542
mintmin <mintmin0320@gmail.com>
2022-12-19 20:14:41 +0900

    Update login function

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -81,7 +81,11 @@ const login = async (req: Request, res: Response) => {
     const token = jwt.sign({ username }, process.env.JWT_SECRET)
 
     //쿠키저장
-    res.set("Set-Cookie", cookie.serialize("token", token));
+    res.set("Set-Cookie", cookie.serialize("token", token, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 7, //일주일
+      path: "/"
+    }))
 
     return res.json({ user, token });
   } catch (error) {
